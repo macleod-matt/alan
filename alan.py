08434a45332a08bc895adaa98eafe5721d06915f
zachharrison <zharrison100@gmail.com>
2024-09-01 15:53:07 -0700

    update paths to check for os

--- alan.py
+++ alan.py
@@ -1,16 +1,19 @@
 import os
 from countCards import CountCards
+from sys import platform
 
 #turn this on to train new model 
 TrainNewModel = False
 cwd = os.getcwd()
 
 if __name__ == "__main__": 
-    model_path = f"{cwd}\\playing-card-classifier\\weights\\best.pt"
+
+    model_path = f"{cwd}\\playing-card-classifier\\weights\\best.pt" if platform == "win32" else f"{cwd}/playing-card-classifier/weights/best.pt"
+    yaml_data = "data\\data.yaml" if platform == "win32" else "data/data.yaml"
     if TrainNewModel:
         trainer = CardModelTrainer(
             model_config="yolov8n.yaml",
-            data_yaml_file="data\\data.yaml",
+            data_yaml_file=f"{yaml_data}",
             project=f"{cwd}",
             experiment="playing-card-classifier",
             batch_size=32,
